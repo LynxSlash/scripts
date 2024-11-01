925e32f5d49f65c0d69e72fb16519f9e39027b9e
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-11-01 05:24:32 +0500

    Update orion.sh

--- orion.sh
+++ orion.sh
@@ -22,6 +22,7 @@ echo "============================"
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
 export TZ=Asia/Dhaka
+export ALLOW_MISSING_DEPENDENCIES=TRUE
 echo "======= Export Done ======"
 
 # Set up build environment
