fdc4f46331196929fe2c0206d20a7ebd9940e101
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-12-10 02:32:35 +0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+export ALLOW_MISSING_DEPENDENCIES=TRUE
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=TRUE
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch pixelstar_a70q-ap2a-eng
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+mka bacon
+
+
+lunch pixelstar_a70q-eng
+
+mka bacon
+
+lunch pixelstar_a70q-userdebugm
