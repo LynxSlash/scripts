8c7cd6004b1bf47524cf5b7e630d97189d933e4a
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-12-17 12:43:34 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-rm -rf device/samsung/a70q
 rm -rf device/samsung/sm6150-common
-rm -rf kernel/samsung/sm6150
-rm -rf vendor/samsung/a70q
-rm -rf vendor/samsung/sm6150-common
-rm -rf hardware/samsung
-git clone -b lineage-21 https://github.com/LineageOS/android_device_samsung_a70q device/samsung/a70q
-git clone -b lineage-21 https://github.com/LineageOS/android_device_samsung_sm6150-common device/samsung/sm6150-common
-git clone -b lineage-21 https://github.com/LineageOS/android_kernel_samsung_sm6150 kernel/samsung/sm6150
-git clone -b lineage-21 https://github.com/a70q-lineage/proprietary_vendor_samsung_a70q vendor/samsung/a70q
-git clone -b lineage-21 https://github.com/a70q-lineage/proprietary_vendor_samsung_sm6150-common vendor/samsung/sm6150-common
-git clone -b lineage-21 https://github.com/LineageOS/android_hardware_samsung hardware/samsung
+git clone -b lineage-21 https://github.com/LynxSlash/android_device_samsung_sm6150-common device/samsung/sm6150-common
 
 # Export
 export BUILD_USERNAME=lynx
