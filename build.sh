29ff4222c98211dbd31e9d64537a39979c7c402f
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-12-23 21:30:05 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-rm -rf device/samsung/sm6150-common
-git clone -b lineage-21 https://github.com/LynxSlash/android_device_samsung_sm6150-common device/samsung/sm6150-common
-
-# Export
-export BUILD_USERNAME=lynx
-export BUILD_HOSTNAME=crave
-export TZ=Asia/Karachi
-echo "======= Export Done ======"
-
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_a70q-ap2a-user
+lunch arrow_a70q-userdebug
+echo "============="
+
+# Make cleaninstall
+make installclean
 echo "============="
 
 # Build rom
