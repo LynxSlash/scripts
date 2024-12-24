29ca8ffa040ad208a84d4d99926991c215ae5386
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-12-24 13:39:16 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Rom source repo
+repo init -u https://github.com/ArrowOS-T/android_manifest.git -b arrow-13.1
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# remove already defined modules
+rm -rf hardware/ril/libril
+# Export
+export TZ=Asia/Karachi
+export ALLOW_MISSING_DEPENDENCIES=TRUE
+echo "======= Export Done ======"
+
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
